da4d3ef6a57aeeeaaca8e787f5c5ab6cc417e3d4
yogi-verma <yogeshvermapys143@gmail.com>
2025-05-05 14:04:29 +0530

    cors solved

--- server.js
+++ server.js
@@ -26,13 +26,19 @@ const io = socketIo(server, {
 const onlineUsers = new Map();
 const disconnectTimers = new Map();
 
+const corsOptions = {
+  origin: "*",
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+}
+
 // Connect to MongoDB
 connectDB();
 
 // Middleware
 app.use(express.json());
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
-app.use(cors());
+app.use(cors(corsOptions));
 
 // Routes
 app.use("/api/members", memberRoutes);
@@ -143,5 +149,5 @@ io.on("connection", (socket) => {
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 server.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
